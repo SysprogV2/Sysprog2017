26a8ef3ea030429daedb1e8770051c91530aed19
0phelia <arty8k@gmail.com>
2017-04-08 14:39:00 +0000

    Reformat code, remove comments

--- Automat/includes/Syntax.h
+++ Automat/includes/Syntax.h
@@ -7,14 +7,14 @@
 
 #ifndef AUTOMAT_INCLUDES_SYNTAX_H_
 #define AUTOMAT_INCLUDES_SYNTAX_H_
-#define SIGN_ARRAY_SZ 14     // we removed one char from @signArray (see below), we added 2 chars '<' and '>'
-#define STATETABLE_WIDTH 18  // we added 2 new states for compounded token &&: '&' and '&&'
-#define STATETABLE_HEIGHT 10 // we added one char '&' to table to look for compounded t0ken '&&', we removed '<' and '>'
+#define SIGN_ARRAY_SZ 14     
+#define STATETABLE_WIDTH 18 
+#define STATETABLE_HEIGHT 10
 #define KEYWORD_NUMBER 6
 #define STATES_NUMBER STATETABLE_WIDTH + STATETABLE_HEIGHT + 1
 class Syntax {
 						 //  STRT_Z, IDEN_Z, INTG_Z,   COLN_Z, EQLS_Z, ASGN_Z, LCLN_Z, LCLL_Z, ASSG_Z, AND1_Z, AND2_Z,	
-	int stateTable[STATETABLE_HEIGHT][STATETABLE_WIDTH] = {									// two new states  ->   /--------------\	
+	int stateTable[STATETABLE_HEIGHT][STATETABLE_WIDTH] = {
 				/*            STRT    ID       INT	      :	     =	   <ANY>	  =:	 =:=	 :=      &      &&      Eof     WSP     *       :*   <comment>  *:    PROH_Z */
 				/* a-Z */	{IDEN_Z, IDEN_Z, STRT_Z,   STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z,  EOF_Z,  STRT_Z, STRT_Z, OPNC_Z, OPNC_Z, STRT_Z, STRT_Z},
 				/* 0-9 */	{INTG_Z, IDEN_Z, INTG_Z,   STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z,  EOF_Z,  STRT_Z, STRT_Z, OPNC_Z, OPNC_Z, STRT_Z, STRT_Z},
@@ -26,30 +26,27 @@ class Syntax {
 				/* EOF */	{EOF_Z,  STRT_Z, STRT_Z,   STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z,  EOF_Z,  STRT_Z, STRT_Z, OPNC_Z, OPNC_Z, STRT_Z, STRT_Z},
 				/* WSP */	{WSP_Z,  STRT_Z, STRT_Z,   STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z,  STRT_Z, STRT_Z, STRT_Z, OPNC_Z, OPNC_Z, STRT_Z, STRT_Z},
 				/* PRH */	{PROH_Z, STRT_Z, STRT_Z,   STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z, STRT_Z,  EOF_Z,  STRT_Z, STRT_Z, OPNC_Z, OPNC_Z, STRT_Z, STRT_Z}
-	};//+sdf&eileen
+	};
 	char* ttypeString[STATES_NUMBER + KEYWORD_NUMBER] = {
-							 "Start     ", "Identifier", "Integer   ", //  0- 3
-							 "Colon     ", "Equals    ", "=:=-sign  ", //  4- 7
-							 "  --=:--  ", "=:=       ", "Assignment", " --eof--  ", //  8-11
-							 "Whitespace", "Multipl   ", "  --:*--  ", " --comm-- ", // 12-15
-							 "  --*:--  ", "Prohibited", " --null-- ", "PlusToken ", // 16-19
-							 "MinusToken", "NotToken  ", "SglAndTok ", "DblAndTok ", // 20-23  replace '&' with SglAndTok and replace '&&' with DblAndTok
-							 "SemicolTok", "Paranth ( ", "Paranth ) ", "Braces {  ", // 24-27 all tokens starting from this line have incremented index from now on
-							 "Braces }  ", "Brackets [", "Brackets ]" ,"IfToken   ", // 28-31
-							 "WhileToken", "INT-Token ", "WRITE-Toke", "ELSE-Token", // 32-35
-							 "READ-Token" 
+							 "Start     ", "Identifier", "Integer   ", "Colon     ", //  0- 3
+							 "Equals    ", "=:=-sign  ", "  --=:--  ", "=:=       ", //  4- 7
+							 "Assignment", " --eof--  ", "Whitespace", "Multipl   ", //  8-11
+							 "  --:*--  ", " --comm-- ", "  --*:--  ", "Prohibited", // 12-15
+							 " --null-- ", "PlusToken ", "MinusToken", "NotToken  ", // 16-19
+							 "SglAndTok ", "DblAndTok ","SemicolTok ", "Paranth ( ", // 20-23  replace '&' with SglAndTok and replace '&&' with DblAndTok
+							 "Paranth ) ", "Braces {  ", "Braces }  ", "Brackets [", // 24-27 all tokens starting from this line have incremented index from now on
+							 "Brackets ]" ,"IfToken   ", "WhileToken", "INT-Token ", // 28-31
+							 "WRITE-Toke", "ELSE-Token", "READ-Token"                // 32-35
+							 
 	};
 	const char signArray[SIGN_ARRAY_SZ] = {'+', '-', '!', ';', '<', '>', '(', ')','{', '}', '[', ']'}; // remove '&' since its got replaced by '&&' , add '<' and '>'
-		// and doesnt belogn to the set of _single chars_
 
 	char* keywordsArray[KEYWORD_NUMBER * 2] = {"if", "IF", "while", "WHILE", "int", "INT", "write", "WRITE", "else", "ELSE", "read", "READ"};
 
 public:
 	int keywordNumber;
 	enum States {STRT_Z, IDEN_Z, INTG_Z, COLN_Z, EQLS_Z, ASGN_Z,
-				 LCLN_Z, LCLL_Z, ASSG_Z, 
-				 AND1_Z, AND2_Z,				       // add two states for '&' and '&&'
-				 EOF_Z,
+				 LCLN_Z, LCLL_Z, ASSG_Z, AND1_Z, AND2_Z, EOF_Z,
 				 WSP_Z,  MULT_Z, OPNC_Z, COMM_Z,
 				 CLSC_Z, PROH_Z, NULL_STATE, PLUS_Z,
 				 MIN_Z,  NOT_Z,  AND_Z,  SEMI_Z,
@@ -57,7 +54,7 @@ public:
 				 BRACKO_Z, BRACKC_Z, IF_Z, WHILE_Z,
 				 INTKEY_Z, WRITE_Z, ELSE_Z, READ_Z
 	};
-	enum Symbols {ANY_LETTER, ANY_DIGIT, MULT_SYMB, COLON_SYMB, EQUALS_SYMB, AND_SYMB, REST_SYMB,  // add AND_SYMB ('&') since it's part of a compound '&&' token, remove '<' and '>' symbols
+	enum Symbols {ANY_LETTER, ANY_DIGIT, MULT_SYMB, COLON_SYMB, EQUALS_SYMB, AND_SYMB, REST_SYMB,
 				  EOF_SYMB, WHITESPACE_SYMB, PROH_SYMB};
 	Syntax();
 	virtual ~Syntax();
