2b87784ff80b04f4c35548e3a235daf198d96234
ehx-v1 <ehxrp@web.de>
2017-05-06 13:55:37 +0200

    Cleaned up obsolete whole-Sequence operations in TokenTypeRegistry, removing the problematic dependency on TokenSequence

--- Scanner/includes/TokenTypeRegistry.h
+++ Scanner/includes/TokenTypeRegistry.h
@@ -8,8 +8,6 @@
 #ifndef SCANNER_INCLUDES_TOKENTYPEREGISTRY_H_
 #define SCANNER_INCLUDES_TOKENTYPEREGISTRY_H_
 
-#include "TokenSequence.h"
-
 class TokenTypeRegistry {
 private:
 	bool* tokenTypes;
@@ -20,13 +18,8 @@ public:
 	void set (Token* token);
 	void unset (Token* token);
 	void toggle (Token* token);
-	// all Tokens from a Sequence modifications do not alter the Sequences
-	void setAll (TokenSequence* sequence);
-	void unsetAll (TokenSequence* sequence);
-	void toggleAll (TokenSequence*  sequence);
 	// checkout operations do not alter anything
 	bool isSet (Token* token);
-	bool areSet (TokenSequence* sequence, CheckoutMode mode);
 	// set operations do not alter the argument (only the call Registry)
 	void intersectWith (TokenTypeRegistry* other);
 	void uniteWith (TokenTypeRegistry* other);
--- Scanner/src/TokenTypeRegistry.cpp
+++ Scanner/src/TokenTypeRegistry.cpp
@@ -30,64 +30,8 @@ void TokenTypeRegistry::toggle (Token* token) {
 	this->tokenTypes [token->getType()] = !this->tokenTypes [token->getType()];
 }
 
-void TokenTypeRegistry::setAll (TokenSequence* sequence) {
-	for (int i = 0; i < sequence->getSize(); i++) {
-		this->set(sequence->tokenAt(i, false));
-	}
-}
-
-void TokenTypeRegistry::unsetAll (TokenSequence* sequence) {
-	for (int i = 0; i < sequence->getSize(); i++) {
-		this->unset(sequence->tokenAt(i, false));
-	}
-}
-
-void TokenTypeRegistry::toggleAll (TokenSequence* sequence) {
-	for (int i = 0; i < sequence->getSize(); i++) {
-		this->toggle(sequence->tokenAt(i, false));
-	}
-}
-
 bool TokenTypeRegistry::isSet (Token* token) {
 	return this->tokenTypes[token->getType()];
-}
-
-bool TokenTypeRegistry::areSet (TokenSequence* sequence, CheckoutMode mode) {
-	int registeredTokens = 0;
-	switch (mode) {
-	case AND:
-		for (int i = 0; i < sequence->getSize(); i++) {
-			if (!this->isSet(sequence->tokenAt(i, false))) return false;
-		}
-		return true;
-	case OR:
-		for (int i = 0; i < sequence->getSize(); i++) {
-			if (this->isSet(sequence->tokenAt(i, false))) return true;
-		}
-		return false;
-	case NOR:
-		for (int i = 0; i < sequence->getSize(); i++) {
-			if (this->isSet(sequence->tokenAt(i, false))) return false;
-		}
-		return true;
-	case NAND:
-		for (int i = 0; i < sequence->getSize(); i++) {
-			if (!this->isSet(sequence->tokenAt(i, false))) return true;
-		}
-		return false;
-	case XOR:
-		for (int i = 0; i < sequence->getSize(); i++) {
-			if (this->isSet(sequence->tokenAt(i, false))) registeredTokens++;
-		}
-		return registeredTokens % 2 == 1;
-	case XNOR:
-		for (int i = 0; i < sequence->getSize(); i++) {
-			if (this->isSet(sequence->tokenAt(i, false))) registeredTokens++;
-		}
-		return registeredTokens % 2 == 0;
-	default:
-		return false;
-	}
 }
 
 void TokenTypeRegistry::intersectWith(TokenTypeRegistry* other) {
