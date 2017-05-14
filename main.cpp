9ffe2b714a02e362c9a8842a3d48b35a0c71405d
ehx-v1 <ehxrp@web.de>
2017-05-14 16:27:58 +0200

    Removed Parser files and adapted main.cpp and MainTest target; added CLion project name and inspection settings to .gitignore

--- main.cpp
+++ main.cpp
@@ -4,12 +4,9 @@
  *  Created on: Nov 11, 2015
  *      Author: arty
  */
-#include "Parser/includes/Parser.h"
+#include "Scanner/includes/Scanner.h"
 
 #include <sys/time.h>
-#include <iostream>
-#include <unistd.h> /* for WRITE */
-#include <fcntl.h>
 
 /* copypasted piece of code for running-time measurements */
 typedef unsigned long long timestamp_t;
@@ -23,37 +20,52 @@ get_timestamp ()
 
 
 int main(int argc, char* argv[]) {
-	std::cout << "Compiler running now (main)\n";
-    timestamp_t t0 = get_timestamp();
+	timestamp_t t0 = get_timestamp();
+	Symboltable* st = new Symboltable();
+	Scanner* s = new Scanner(argv[1], st);
+	Token* t;
+	Syntax* syntax = new Syntax();
+	std::cout << "processing..." << std::endl;
 
-	if (argc < 1) return EXIT_FAILURE;
-
-	/* unlike previous version, this is self-made,
-	 * but the test should run correctly
-	 */
-
-
-	int compile_error = 0;
-	try {
-
-		Parser* parser = new Parser(argv[0]);
-		ParseTree* tree = parser->parse();
-		if (tree->typeCheck()) {
-			compile_error = 0;
-			tree->makeCode();
+	/* check if all arguments are given */
+	if (argc == 3) {
+		std::ofstream output_file;
+		output_file.open(argv[2]);
+		if (output_file.is_open()) {
+			while ((t = s->nextToken())) {
+				/* console output */
+				if (t->getType() == Syntax::PROH_Z) {
+					std::cout << t->getErrorMessage()
+							  << " Line: " << t->getLine()
+							  << " Column: " << t->getColumn();
+					if (t->getSymbol() != nullptr) {
+						std::cout << " Symbol: " << t->getSymbol();
+					}
+					std::cout << std::endl;
+					/* print everything else to the output file */
+				} else if (t->getType() != Syntax::WSP_Z) {
+					output_file
+							<< syntax->getTokenTypeAsChar(t->getType())
+							<< " Line: " << t->getLine()
+							<< " Column: " << t->getColumn();
+					if (t->getType() == Syntax::IDEN_Z) {
+						output_file << " Lexem: " << t->getLexem();
+					} else if (t->getType() == Syntax::INTG_Z) {
+						output_file << " Value: " << t->getValue();
+					}
+					output_file << std::endl;
+				}
+			}
+			std::cout << "stop" << std::endl
+					  << "Exec time: " << ((get_timestamp() - t0) / 1000000.0L) << std::endl;
+			output_file.close();
 		} else {
-			compile_error = 1;
+			std::cout << "Error: couldn't open output file. Exiting..." << std::endl;
 		}
-	} catch (int error) {
-		cerr << argv[1] << "could not be compiled successfully." << endl;
-		compile_error = error;
+	} else {
+		std::cout << "Error: path to input and/or output file is not given. Exiting..." << std::endl;
 	}
-
-
-
-
-	std::cout << "Time: " << (get_timestamp() - t0) / 1000000.0L << "secs"  << std::endl;
-	return compile_error;
+	return EXIT_SUCCESS;
 }
 
 
