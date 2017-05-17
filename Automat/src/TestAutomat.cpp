5803daedd6146f3d893280a0ce51ace2d3d9bba5
dojo1017 <dojo1017@hs-karlsruhe.de>
2017-05-17 11:53:28 +0200

    test

--- Automat/src/TestAutomat.cpp
+++ Automat/src/TestAutomat.cpp
@@ -1,205 +1,147 @@
 #define CATCH_CONFIG_MAIN
+
 #include "../includes/Automat.h"
-#include <iostream>
 #include "../../CatchLib/includes/catch.hpp"
-#include <string.h>
-#define WSP_Z 12
-#define CLSC_Z 16
 
-/**
- * Returns next lexem found in @inputString
- * 
- * @automat
- * @inputString
- */
-const char* getNextLexem(Automat* automat, char* inputString) {
-	int i = 0;
-	char nextChar = inputString[i];
-	while (nextChar != '\0') {
-		int back_steps = automat->read(nextChar);
-		// adjust out pointer position & read the next symbol from input
-		i = i + 1 - back_steps;
-		nextChar = inputString[i];
-		// reset automat iff a new lexem was found during processing
-		if (automat->isLexemReady()) {
-			//std::cout << "Lexem '" << automat->getLexem() << "' is ready. State: " << automat->getFinalState() << "   Length = " << automat->getLexemLength() << "   LINE: " << automat->getLine()  << "   COL: " << automat->getColumn() << std::endl;
-			return automat->getLexem();
-			automat->reset();
-		}
-	}
-}
 
-//							NOTICE
-// following primitive methods are not tested since they 
+// NOTICE
+// following primitive methods are not tested since they
 // merely retrieve values from the existing tables/arrays:
-// 
+//
 // Syntax::getTokenTypeAsChar(int num);
 // Syntax::getState(int i, int j)
 
+TEST_CASE("SYNTAX.CPP TEST -> matches()", "[char]") {
+    Syntax *syntax = new Syntax();
 
-TEST_CASE( "AUTOMAT.CPP TEST: Lexems are processed", "[char]") {
-	// TEST CASE SETUP
-	Automat* automat = new Automat();
-	
-	// #1
-	char* inputString = "&&&other+=:+probably+:=+:=:qeek+k#oo+=:=:+*\0";
-	REQUIRE( !strcmp(getNextLexem(automat, inputString), "&&") );
+    SECTION("When two strings are equal, they should match") {
+        REQUIRE(syntax->matches("\0", "\0"));
+        REQUIRE(syntax->matches("ag\0", "ag\0"));
+        REQUIRE(syntax->matches("ag", "ag"));
+        REQUIRE(syntax->matches("!@$#%@$^^#&^&*(^((){}\0", "!@$#%@$^^#&^&*(^((){}\0"));
+    }
+    SECTION("When two strings are not equal, they should not match") {
+        REQUIRE(!syntax->matches("a\0", "A\0"));
+        REQUIRE(!syntax->matches("if", "iF"));
+    }
+}
+
+TEST_CASE("SYNTAX.CPP TEST -> ifKeyword()", "[char]") {
+    Syntax *syntax = new Syntax();
+
+    SECTION("lower case keywords should be recognized") {
+        REQUIRE(syntax->ifKeyword("if"));
+        REQUIRE(syntax->ifKeyword("while"));
+    }
+    SECTION("upper case keywords should be recognized") {
+        REQUIRE(syntax->ifKeyword("IF"));
+        REQUIRE(syntax->ifKeyword("WHILE"));
+    }
+    SECTION("mixed case keywords should NOT be recognized") {
+        REQUIRE(syntax->ifKeyword("iF") == -1);
+        REQUIRE(syntax->ifKeyword("WHILe") == -1);
+    }
+}
+
+TEST_CASE("SYNTAX.CPP TEST -> isPacked()", "[char]") {
+    Syntax *syntax = new Syntax();
+
+    SECTION("Known symbols should be recognized") {
+        REQUIRE(syntax->isPacked('+'));
+        REQUIRE(syntax->isPacked('-'));
+        REQUIRE(syntax->isPacked('!'));
+        REQUIRE(syntax->isPacked(';'));
+        REQUIRE(syntax->isPacked('<'));
+        REQUIRE(syntax->isPacked('>'));
+        REQUIRE(syntax->isPacked('('));
+        REQUIRE(syntax->isPacked(')'));
+        REQUIRE(syntax->isPacked('{'));
+        REQUIRE(syntax->isPacked('}'));
+        REQUIRE(syntax->isPacked('['));
+        REQUIRE(syntax->isPacked(']'));
+    }
+    SECTION("Unknown symbols should not be recognized") {
+        REQUIRE(syntax->isPacked('&') == -1);
+        REQUIRE(syntax->isPacked(':') == -1);
+        REQUIRE(syntax->isPacked('=') == -1);
+    }
 }
 
 
-TEST_CASE( "SYNTAX.CPP TEST -> matches()", "[char]") {
-	// TEST CASE SETUP
-	Syntax* syntax = new Syntax();
-	
-	// #1
-	REQUIRE( syntax->matches("\0", "\0") );
-	
-	// #2
-	REQUIRE( syntax->matches("a\0", "A\0") == false);
-	
-	// #3
-	REQUIRE( syntax->matches("ag\0", "ag\0") );
-	
-	// #4
-	REQUIRE( syntax->matches("ag", "ag") );
-	
-	// #5
-	REQUIRE( syntax->matches("!@$#%@$^^#&^&*(^((){}\0", "!@$#%@$^^#&^&*(^((){}\0") );
-	
-	// #6
-	REQUIRE( syntax->matches("if", "iF") == false);
+TEST_CASE("STACK.CPP TEST -> push(); trim(); get(); flush()", "[char]") {
+    Stack *stack = new Stack();
+
+    SECTION("empty") {
+        REQUIRE(!strcmp(stack->get(), ""));
+
+        stack->push('a');
+        stack->push('b');
+        stack->push('c');
+
+        SECTION("abc") {
+            REQUIRE(!strcmp(stack->get(), "abc"));
+
+            stack->trim(0);
+            REQUIRE(!strcmp(stack->get(), "abc"));
+            stack->trim(2);
+            REQUIRE(!strcmp(stack->get(), "a"));
+            stack->trim(1);
+            REQUIRE(!strcmp(stack->get(), ""));
+        }
+
+        SECTION("abc") {
+            REQUIRE(!strcmp(stack->get(), "abc"));
+
+            stack->flush();
+            REQUIRE(!strcmp(stack->get(), ""));
+
+        }
+    }
 }
 
-TEST_CASE( "SYNTAX.CPP TEST -> ifKeyword()", "[char]") {
-	// TEST CASE SETUP
-	Syntax* syntax = new Syntax();
-	
-	// #1
-	REQUIRE( syntax->ifKeyword("if") );
+TEST_CASE("AUTOMAT.CPP TEST -> read(char currentChar); reset()", "[char]") {
+    Automat *automat = new Automat();
 
-	// #2
-	REQUIRE( syntax->ifKeyword("iF") == -1);
-	
-	// #3
-	REQUIRE( syntax->ifKeyword("IF"));
-	
-	// #4
-	REQUIRE( syntax->ifKeyword("while"));
-	
-	// #5
-	REQUIRE( syntax->ifKeyword("WHILE"));
-	
-	// #6
-	REQUIRE( syntax->ifKeyword("WHILe") == -1);
+    SECTION("")
+    automat->reset();
+    automat->read('=');
+    automat->read(':');
+    int result1 = automat->read('x');
+    REQUIRE(result1 == 2);
+
+    // #2
+    automat->reset();
+    automat->read(':');
+    automat->read('=');
+    int result2 = automat->read('x');
+    REQUIRE(result2 == 1);
+
+    // #3
+    automat->reset();
+    automat->read('=');
+    automat->read(':');
+    automat->read('=');
+    int result3 = automat->read('x');
+    REQUIRE(result3 == 1);
+
+    // #4
+    automat->reset();
+    int result4 = automat->read('x');
+    REQUIRE(result4 == 0);
 }
 
-TEST_CASE( "SYNTAX.CPP TEST -> isPacked()", "[char]") {
-	// TEST CASE SETUP
-	Syntax* syntax = new Syntax();
-	const char *signArray = new const char[14]
-		{'+', '-', '!', ';', '<', '>', '(', ')','{', '}', '[', ']'};
-	
-	// #1 - #13
-	REQUIRE( syntax->isPacked( signArray[0]) );
-	REQUIRE( syntax->isPacked( signArray[1]) );
-	REQUIRE( syntax->isPacked( signArray[2]) );
-	REQUIRE( syntax->isPacked( signArray[3]) );
-	REQUIRE( syntax->isPacked( signArray[4]) );
-	REQUIRE( syntax->isPacked( signArray[5]) );
-	REQUIRE( syntax->isPacked( signArray[6]) );
-	REQUIRE( syntax->isPacked( signArray[7]) );
-	REQUIRE( syntax->isPacked( signArray[8]) );
-	REQUIRE( syntax->isPacked( signArray[9]) );
-	REQUIRE( syntax->isPacked( signArray[13]) );
-	
-	// #13 - #15
-	REQUIRE( syntax->isPacked( '&') == -1 );
-	REQUIRE( syntax->isPacked( ':') == -1 );
-	REQUIRE( syntax->isPacked( '=') == -1 );
-}
+TEST_CASE("AUTOMAT.CPP TEST -> updatePos(char c); :getLine(); getColumn()", "[char]") {
+    Automat *automat = new Automat();
 
-
-TEST_CASE( "STACK.CPP TEST -> push(); trim(); get(); flush()", "[char]") {
-	// TEST CASE SETUP
-	Stack* stack = new Stack();
-
-	// #1
-	stack->push('a');
-	stack->push('b');
-	stack->push('c');
-	REQUIRE( !strcmp(stack->get(), "abc") );
-	
-	// #2
-	stack->trim(0);
-	REQUIRE( !strcmp(stack->get(), "abc") );
-	
-	// #3
-	stack->trim(2);
-	REQUIRE( !strcmp(stack->get(), "a") );
-	
-	// #4
-	stack->trim(1);
-	REQUIRE( !strcmp(stack->get(), "") );
-	
-	// #5
-	stack->flush();
-	REQUIRE( !strcmp(stack->get(), "") );
-	
-	// #6
-	// stack->push('§');
-	//REQUIRE( !strcmp(stack->get(), "§") );
-	
-	// #7
-	stack->trim(1);
-	REQUIRE( !strcmp(stack->get(), "") );
-}
-
-TEST_CASE( "AUTOMAT.CPP TEST -> read(char currentChar); reset()", "[char]") {
-	// TEST CASE SETUP
-	Automat* automat = new Automat();
-	
-	// #1
-	automat->reset();
-	automat->read('=');
-	automat->read(':');
-	int result1 = automat->read('x');
-	REQUIRE( result1 == 2 );
-	
-	// #2
-	automat->reset();
-	automat->read(':');
-	automat->read('=');
-	int result2 = automat->read('x');
-	REQUIRE( result2 == 1 );
-	
-	// #3
-	automat->reset();
-	automat->read('=');
-	automat->read(':');
-	automat->read('=');
-	int result3 = automat->read('x');
-	REQUIRE( result3 == 1 );
-	
-	// #4
-	automat->reset();
-	int result4 = automat->read('x');
-	REQUIRE( result4 == 0 );
-}
-
-TEST_CASE( "AUTOMAT.CPP TEST -> updatePos(char c); :getLine(); getColumn()", "[char]") {
-	// TEST CASE SETUP
-	Automat* automat = new Automat();
-	
-	// #1
-	automat->reset();
-	automat->read('\n');
-	automat->read('\n');
-	automat->read('\n');
-	automat->read('a');
-	automat->read('!');
-	int line1 = automat->getLine();
-	int col1 = automat->getColumn();
-	REQUIRE( line1 == 3 );
-	REQUIRE( col1 == 2 );
-
+    // #1
+    automat->reset();
+    automat->read('\n');
+    automat->read('\n');
+    automat->read('\n');
+    automat->read('a');
+    automat->read('!');
+    int line1 = automat->getLine();
+    int col1 = automat->getColumn();
+    REQUIRE(line1 == 3);
+    REQUIRE(col1 == 2);
 }
--- Scanner/src/Scanner.cpp
+++ Scanner/src/Scanner.cpp
@@ -6,33 +6,30 @@
  */
 #include "../includes/Scanner.h"
 
-#define CONV_ERR "error: cannot convert given CHAR* to LONG INT"
-#define CONV_ERR_OVERFLOW "error: given integer is too big to be converted to LONG INT"
-
-Scanner::Scanner(char *filename): Scanner(filename, new Symboltable()) {
+Scanner::Scanner(char *filename) : Scanner(filename, new Symboltable()) {
 }
 
-Scanner::Scanner(char *filename, Symboltable* st) {
-	stab = st;
-	buffer = new Buffer(filename);
-	automat = new Automat();
-	syntax = new Syntax();
-	cToken = nullptr;
-	idenInt =  new TokenTypeRegistry();
-	Token* idenToken = new Token(1, 0, 0);
-	Token* intToken = new Token (2, 0, 0);
-	idenInt->set(idenToken);
-	idenInt->set(intToken);
-	delete idenToken;
-	delete intToken;
+Scanner::Scanner(char *filename, Symboltable *st) {
+    stab = st;
+    buffer = new Buffer(filename);
+    automat = new Automat();
+    syntax = new Syntax();
+    cToken = nullptr;
+    idenInt = new TokenTypeRegistry();
+    Token *idenToken = new Token(1, 0, 0);
+    Token *intToken = new Token(2, 0, 0);
+    idenInt->set(idenToken);
+    idenInt->set(intToken);
+    delete idenToken;
+    delete intToken;
 }
 
 Scanner::~Scanner() {
-	delete buffer;
-	delete automat;
-	delete syntax;
-	if (!this->idenInt->isSet(this->cToken)) delete this->cToken;
-	delete this->idenInt;
+    delete buffer;
+    delete automat;
+    delete syntax;
+    if (!this->idenInt->isSet(this->cToken)) delete this->cToken;
+    delete this->idenInt;
 }
 
 /*
@@ -41,106 +38,105 @@ Scanner::~Scanner() {
  * @return current token
  */
 Token *Scanner::nextToken() {
-	//std::cout << "debuging *** Scanner::nextToken STRT" << std::endl;
+    // std::cout << "debuging *** Scanner::nextToken STRT" << std::endl;
 
-	char currentChar;
-	int finalState = 0;
+    char currentChar;
+    int finalState = 0;
 
-	//std::cout << "debuging *** Scanner::nextToken DO-STRT" << std::endl;
-	/* run automat and feed it char by char, till any lexem is found */
-	if (buffer->isEnd()) {
-		return nullptr;
-	}
+    // std::cout << "debuging *** Scanner::nextToken DO-STRT" << std::endl;
+    /* run automat and feed it char by char, till any lexem is found */
+    if (buffer->isEnd()) {
+        return nullptr;
+    }
 
-	do {
-		currentChar = buffer->getChar();
-		int back_steps = automat->read(currentChar);
-		finalState = automat->getFinalState();
-		buffer->ungetChar(back_steps);
-		if (automat->isLexemReady() && (finalState == Syntax::WSP_Z || finalState == Syntax::CLSC_Z)) {
-			automat->reset();
-		}
-	} while (!buffer->isEnd() && currentChar != '\0' &&  !automat->isLexemReady());
-	//std::cout << "debuging *** Scanner::nextToken DO-NED" << std::endl;
+    do {
+        currentChar = buffer->getChar();
+        int back_steps = automat->read(currentChar);
+        finalState = automat->getFinalState();
+        buffer->ungetChar(back_steps);
+        if (automat->isLexemReady() && (finalState == Syntax::WSP_Z || finalState == Syntax::CLSC_Z)) {
+            automat->reset();
+        }
+    } while (!buffer->isEnd() && currentChar != '\0' && !automat->isLexemReady());
+    // std::cout << "debuging *** Scanner::nextToken DO-NED" << std::endl;
 
-	/* save all information about the lexem */
-	const char* lexem = automat->getLexem();
-	int lexemLength = automat->getLexemLength();
-	int line = automat->getLine();
-	int col = automat->getColumn();
-	int tokenType = mapStateToType(finalState, lexem);
+    /* save all information about the lexem */
+    const char *lexem = automat->getLexem();
+    int lexemLength = automat->getLexemLength();
+    int line = automat->getLine();
+    int col = automat->getColumn();
+    int tokenType = mapStateToType(finalState, lexem);
 
-	/* create Token */
-	Token* t = new Token(tokenType, line, col);
+    /* create Token */
+    Token *t = new Token(tokenType, line, col);
 
-	/* add additional information to the token */
-	Information* info;
-	if (tokenType == Syntax::IDEN_Z) {
-		info = stab->lookup(lexem);
-		if (info == nullptr) {
-			SymtabEntry* entry = stab->insert(lexem, lexemLength);
-			info = entry->getInfo();
-		}
-		t->setInformation(info);
-	} else if (tokenType == Syntax::AND1_Z || tokenType == Syntax::PROH_Z) {
-		t->setType(Syntax::PROH_Z);
-		t->setErrorMessage("unknown token");
-		t->setSymbol(lexem[0]);
-	} else if (tokenType == Syntax::INTG_Z) {
-		getNumberToken(lexem, t);
-	}
+    /* add additional information to the token */
+    Information *info;
+    if (tokenType == Syntax::IDEN_Z) {
+        info = stab->lookup(lexem);
+        if (info == nullptr) {
+            SymtabEntry *entry = stab->insert(lexem, lexemLength);
+            info = entry->getInfo();
+        }
+        t->setInformation(info);
+    } else if (tokenType == Syntax::AND1_Z || tokenType == Syntax::PROH_Z) {
+        t->setType(Syntax::PROH_Z);
+        t->setErrorMessage("unknown token");
+        t->setSymbol(lexem[0]);
+    } else if (tokenType == Syntax::INTG_Z) {
+        getNumberToken(lexem, t);
+    }
 
-	/* now we can reset automat */
-	automat->reset();
-	//std::cout << "debuging *** Scanner::nextToken END" << std::endl;
+    /* now we can reset automat */
+    automat->reset();
+    // std::cout << "debuging *** Scanner::nextToken END" << std::endl;
 
-	/* if we need to finish already*/
-	if (currentChar == '\0') {
-		return nullptr;
-	} else {
-		if (this->cToken != nullptr && !this->idenInt->isSet(this->cToken)) {
+    /* if we need to finish already*/
+    if (currentChar == '\0') {
+        return nullptr;
+    } else {
+        if (this->cToken != nullptr && !this->idenInt->isSet(this->cToken)) {
             delete this->cToken;
         }
 
-		this->cToken = t;
-		return t;
-	}
+        this->cToken = t;
+        return t;
+    }
 }
 
-Token* Scanner::currentToken() {
-	return this->cToken;
+Token *Scanner::currentToken() {
+    return this->cToken;
 }
 
 /*
  * determine the current token type relying on STATE and LEXEM
  * @return token's type
  */
-int Scanner::mapStateToType(int state, const char* lexem) {
-	char symbol = lexem[0];
-	int tType = state;
-	if (state == Syntax::ASGN_Z) {
-		tType = syntax->isPacked(symbol);
-	} else if (state == Syntax::IDEN_Z) {
-		int tmp = syntax->ifKeyword(lexem);
-		if (tmp > 0) tType = tmp; // 30..35
-	}
-	return tType;
+int Scanner::mapStateToType(int state, const char *lexem) {
+    char symbol = lexem[0];
+    int tType = state;
+    if (state == Syntax::ASGN_Z) {
+        tType = syntax->isPacked(symbol);
+    } else if (state == Syntax::IDEN_Z) {
+        int tmp = syntax->ifKeyword(lexem);
+        if (tmp > 0) tType = tmp;  // 30..35
+    }
+    return tType;
 }
 
 /*
  * assigns a decimal value of given char pointer to given token
  * @return void
  */
-void Scanner::getNumberToken(const char* lexem, Token* t) {
-	errno = 0;
-	long int value = strtol(lexem, 0, 10);
-	if (errno == ERANGE) {
-		t->setType(Syntax::PROH_Z);
-		t->setErrorMessage("error: given integer is too big to be converted to LONG INT");
-		t->setSymbol(nullptr);
-		errno = 0;
-	} else {
-		t->setValue(value);
-	}
-	
+void Scanner::getNumberToken(const char *lexem, Token *t) {
+    errno = 0;
+    int64_t value = strtol(lexem, 0, 10);
+    if (errno == ERANGE) {
+        t->setType(Syntax::PROH_Z);
+        t->setErrorMessage("error: given integer is too big to be converted to LONG INT");
+        t->setSymbol(nullptr);
+        errno = 0;
+    } else {
+        t->setValue(value);
+    }
 }
