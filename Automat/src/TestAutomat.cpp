ef7b5b9ade13ce24e15130606ad6bd55ec655c17
dojo1017 <dojo1017@hs-karlsruhe.de>
2017-06-05 20:42:16 +0200

    fix automat tests

--- Automat/src/TestAutomat.cpp
+++ Automat/src/TestAutomat.cpp
@@ -2,6 +2,28 @@
 #include "../../Parser/includes/ParseTree.h"
 #include "gtest/gtest.h"
 
+/**
+ * Returns next lexem found in @inputString
+ *
+ * @automat
+ * @inputString
+ */
+const char *getNextLexem(Automat *automat, char *inputString) {
+    int i = 0;
+    char nextChar = inputString[i];
+    while (nextChar != '\0') {
+        int back_steps = automat->read(nextChar);
+        // adjust out pointer position & read the next symbol from input
+        i = i + 1 - back_steps;
+        nextChar = inputString[i];
+        // reset automat iff a new lexem was found during processing
+        if (automat->isLexemReady()) {
+            //std::cout << "Lexem '" << automat->getLexem() << "' is ready. State: " << automat->getFinalState() << "   Length = " << automat->getLexemLength() << "   LINE: " << automat->getLine()  << "   COL: " << automat->getColumn() << std::endl;
+            return automat->getLexem();
+        }
+    }
+}
+
 // NOTICE
 // following primitive methods are not tested since they
 // merely retrieve values from the existing tables/arrays:
@@ -9,21 +31,30 @@
 // Syntax::getTokenTypeAsChar(int num);
 // Syntax::getState(int i, int j)
 
+TEST(automat, read_isLexemReady_getLexem) {
+    // TEST CASE SETUP
+    Automat *automat = new Automat();
+
+    // #1
+    char *inputString = "&&&other+=:+probably+:=+:=:qeek+k#oo+=:=:+*\0";
+    EXPECT_TRUE(!strcmp(getNextLexem(automat, inputString), "&&"));
+}
+
 TEST(automat, matches) {
     Syntax *syntax = new Syntax();
 
     // When two strings are equal, they should match
     EXPECT_TRUE(syntax->matches("\0", "\0"));
+
     EXPECT_TRUE(syntax->matches("ag\0", "ag\0"));
     EXPECT_TRUE(syntax->matches("ag", "ag"));
     EXPECT_TRUE(syntax->matches("!@$#%@$^^#&^&*(^((){}\0", "!@$#%@$^^#&^&*(^((){}\0"));
 
-    // When two strings are not equal, they should not match
     EXPECT_FALSE(syntax->matches("a\0", "A\0"));
     EXPECT_FALSE(syntax->matches("if", "iF"));
 }
 
-TEST(automat, ifKeyword) {
+TEST(syntax, ifKeyword) {
     Syntax *syntax = new Syntax();
 
     // lower case keywords should be recognized
@@ -40,22 +71,24 @@ TEST(automat, ifKeyword) {
 
 }
 
-TEST(automat, isPacked) {
+TEST(syntax, isPacked) {
+    // TEST CASE SETUP
     Syntax *syntax = new Syntax();
+    const char *signArray = new const char[14]
+            {'+', '-', '!', ';', '<', '>', '(', ')', '{', '}', '[', ']'};
 
-    // Known symbols should be recognized
-    EXPECT_TRUE(syntax->isPacked('+'));
-    EXPECT_TRUE(syntax->isPacked('-'));
-    EXPECT_TRUE(syntax->isPacked('!'));
-    EXPECT_TRUE(syntax->isPacked(';'));
-    EXPECT_TRUE(syntax->isPacked('<'));
-    EXPECT_TRUE(syntax->isPacked('>'));
-    EXPECT_TRUE(syntax->isPacked('('));
-    EXPECT_TRUE(syntax->isPacked(')'));
-    EXPECT_TRUE(syntax->isPacked('{'));
-    EXPECT_TRUE(syntax->isPacked('}'));
-    EXPECT_TRUE(syntax->isPacked('['));
-    EXPECT_TRUE(syntax->isPacked(']'));
+    // #1 - #13
+    EXPECT_TRUE(syntax->isPacked(signArray[0]));
+    EXPECT_TRUE(syntax->isPacked(signArray[1]));
+    EXPECT_TRUE(syntax->isPacked(signArray[2]));
+    EXPECT_TRUE(syntax->isPacked(signArray[3]));
+    EXPECT_TRUE(syntax->isPacked(signArray[4]));
+    EXPECT_TRUE(syntax->isPacked(signArray[5]));
+    EXPECT_TRUE(syntax->isPacked(signArray[6]));
+    EXPECT_TRUE(syntax->isPacked(signArray[7]));
+    EXPECT_TRUE(syntax->isPacked(signArray[8]));
+    EXPECT_TRUE(syntax->isPacked(signArray[9]));
+    EXPECT_TRUE(syntax->isPacked(signArray[13]));
 
     // Unknown symbols should not be recognized
     EXPECT_TRUE(syntax->isPacked('&') == -1);
@@ -65,41 +98,48 @@ TEST(automat, isPacked) {
 }
 
 
-TEST(automat, push_trim_get_flush) {
+TEST(stack, push_trim_get_flush) {
+    // TEST CASE SETUP
     Stack *stack = new Stack();
 
-    // empty
-    EXPECT_FALSE(strcmp(stack->get(), ""));
-
+    // #1
     stack->push('a');
     stack->push('b');
     stack->push('c');
 
-    // abc
-    EXPECT_FALSE(strcmp(stack->get(), "abc"));
-
+    // #2
     stack->trim(0);
-    EXPECT_FALSE(strcmp(stack->get(), "abc"));
+    EXPECT_TRUE(!strcmp(stack->get(), "abc"));
+
+    // #3
     stack->trim(2);
-    EXPECT_FALSE(strcmp(stack->get(), "a"));
+    EXPECT_TRUE(!strcmp(stack->get(), "a"));
+
+    // #4
     stack->trim(1);
-    EXPECT_FALSE(strcmp(stack->get(), ""));
+    EXPECT_TRUE(!strcmp(stack->get(), ""));
 
-
-    // abc
-    EXPECT_FALSE(strcmp(stack->get(), "abc"));
-
+    // #5
     stack->flush();
-    EXPECT_FALSE(strcmp(stack->get(), ""));
+    EXPECT_TRUE(!strcmp(stack->get(), ""));
+
+    // #6
+    // stack->push('§');
+    //REQUIRE( !strcmp(stack->get(), "§") );
+
+    // #7
+    stack->trim(1);
+    EXPECT_TRUE(!strcmp(stack->get(), ""));
 
 }
 
 
 TEST(automat, read_reset) {
+    // TEST CASE SETUP
     Automat *automat = new Automat();
 
+    // #1
     automat->reset();
-
     automat->read('=');
     automat->read(':');
     int result1 = automat->read('x');
@@ -107,7 +147,6 @@ TEST(automat, read_reset) {
 
     // #2
     automat->reset();
-
     automat->read(':');
     automat->read('=');
     int result2 = automat->read('x');
@@ -115,7 +154,6 @@ TEST(automat, read_reset) {
 
     // #3
     automat->reset();
-
     automat->read('=');
     automat->read(':');
     automat->read('=');
@@ -124,17 +162,16 @@ TEST(automat, read_reset) {
 
     // #4
     automat->reset();
-
     int result4 = automat->read('x');
     EXPECT_EQ(result4, 0);
 }
 
 TEST(automat, updatePos_getLine_getColumn) {
+    // TEST CASE SETUP
     Automat *automat = new Automat();
 
     // #1
     automat->reset();
-
     automat->read('\n');
     automat->read('\n');
     automat->read('\n');
@@ -144,4 +181,5 @@ TEST(automat, updatePos_getLine_getColumn) {
     int col1 = automat->getColumn();
     EXPECT_EQ(line1, 3);
     EXPECT_EQ(col1, 2);
+
 }
